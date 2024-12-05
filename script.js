72140ba13909d5499884c8cd38b3512ada1853b1
marwat98 <marc.wat001@gmail.com>
2024-12-05 14:25:36 +0100

    Fixed problem with bracket function

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-let display = document.getElementsById("display");
 
-function clear(){
-    display.value = "";
-};
-function addToDisplay(input){
+const display = document.getElementById("display");
+const result = document.getElementById("result");
+
+let leftBracket = 0;
+let rightBracket = 0;
+
+function clearDisplay() {
+    display.value = " ";
+    result.value = " ";
+}
+
+function addToDisplay(input) {
     display.value += input;
+}
+function bracetLeft(input = "("){
+    if (display.value.slice(-1) !== "(") {
+        display.value += input;
+        leftBracket++; 
+    }
 };
-function equals(){
-    display.value = eval(display.value);
+
+function bracetRight(input = ")"){
+    if(rightBracket < leftBracket){
+        display.value += input;
+        rightBracket++; 
+    } else{
+        alert("Nie można dodać zamykającego nawiasu przed otwierającym");
+    }
 };
+
+function equals() {
+    try {
+        result.value = math.evaluate(display.value);
+    } catch (e) {
+        display.value = "Error";
+    }
+}
